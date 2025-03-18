89849f07c314901cfbbc4295de6034ed238679d2
Rodrigo <rodrigorocha1032@gmail.com>
2025-03-18 16:10:03 -0300

    organização e mudanças na contagem de objetos

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Para utilizar é necessário instalar as dependências utilizando pip e então executar: python3 main.py
 
-Primeiramente todos os métodos disponíveis (somam um total de 8 métodos) irão aparecer como opção. Com o método selecionado é possível então escolher a imagem para ser processada por tal método.
+Primeiramente todos os métodos disponíveis (somam um total de 7 métodos) irão aparecer como opção. Com o método selecionado é possível então escolher a imagem para ser processada por tal método.
 
 Todas as imagens do diretório `imagens` são listadas, então caso seja necessário testar com outra imagem, é apenas necessário adicioná-la ao diretório.
+
+Separadamente, é possível executar: python3 contar_objetos.py (neste caso a imagem deve ser colocada manualmente no código)
--- contar_objetos.py
+++ contar_objetos.py
@@ -1,56 +1,56 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import otsu
 import watershed
+from skimage import io, color, filters, measure, morphology, img_as_ubyte
+from skimage.draw import rectangle_perimeter
 
 def detectar_objetos(imagem_path, min_area=100, kernel_size=5):
-    # 1. Carregar imagem e converter para escala de cinza
-    img = cv2.imread(imagem_path)
-    cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    # 2. Aplicar Otsu para binarização automática
-    _, binaria = cv2.threshold(cinza, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    
-    # 3. Pós-processamento morfológico (fechamento para remover ruídos)
-    kernel = np.ones((kernel_size, kernel_size), np.uint8)
-    processada = cv2.morphologyEx(binaria, cv2.MORPH_CLOSE, kernel)
-    
-    # 4. Encontrar contornos dos objetos
-    contornos, _ = cv2.findContours(processada, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    # 5. Filtrar e desenhar resultados
-    resultado = img.copy()
+    # Carregar a imagem e converter para escala de cinza
+    img = io.imread(imagem_path)
+    if img.ndim == 3:
+        cinza = color.rgb2gray(img)
+    else:
+        cinza = img
+
+    # Aplicar Otsu para binarização automática (inverso igual ao THRESH_BINARY_INV)
+    limiar = filters.threshold_otsu(cinza)
+    binaria = cinza < limiar  # inverte para fundo preto e objeto branco
+
+    # Fechamento morfológico para remover ruídos
+    binaria = morphology.closing(binaria, morphology.square(kernel_size))
+
+    # Rotular objetos conectados
+    rotulado = measure.label(binaria)
+    props = measure.regionprops(rotulado)
+
+    # Filtrar por área e registrar objetos
     objetos = []
-    
-    for cnt in contornos:
-        area = cv2.contourArea(cnt)
-        if area > min_area:
-            # Retângulo delimitador
-            x, y, w, h = cv2.boundingRect(cnt)
-            
-            # Armazenar dados do objeto
+    resultado = img_as_ubyte(color.gray2rgb(cinza))  # Converte para desenhar colorido
+
+    for prop in props:
+        if prop.area > min_area:
+            minr, minc, maxr, maxc = prop.bbox
             objetos.append({
-                'posicao': (x, y),
-                'dimensoes': (w, h),
-                'area': area
+                'posicao': (minc, minr),
+                'dimensoes': (maxc - minc, maxr - minr),
+                'area': prop.area
             })
-            
-            # Desenhar contorno e retângulo
-            cv2.drawContours(resultado, [cnt], -1, (0, 255, 0), 2)
-            cv2.rectangle(resultado, (x, y), (x+w, y+h), (0, 0, 255), 2)
-    
+            # Desenhar retângulo vermelho
+            rr, cc = rectangle_perimeter(start=(minr, minc), end=(maxr-1, maxc-1), shape=resultado.shape)
+            resultado[rr, cc] = [255, 0, 0]  # vermelho
+
     return resultado, objetos
 
 # Exemplo de uso
-imagem_path = f"7.jpg"
-watershed.all_watershed(imagem_path)
-otsu.all_otsu("../resultados/watershed.jpg")
-imagem_resultado, objetos_detectados = detectar_objetos("./resultados/otsu.jpg")
+imagem_path = f"./imagens_treinamento/7.jpg"
+watershed.all_watershed("../imagens_treinamento/7.jpg")
+imagem_resultado, objetos_detectados = detectar_objetos("./resultados/watershed.jpg")
 
 # Mostrar resultados
 plt.figure(figsize=(12, 6))
-plt.subplot(121), plt.imshow(cv2.cvtColor(cv2.imread(f"./imagens/{imagem_path}"), cv2.COLOR_BGR2RGB))
+plt.subplot(121), 
+plt.imshow(cv2.cvtColor(cv2.imread(f"{imagem_path}"), cv2.COLOR_BGR2RGB))
 plt.title('Imagem Original')
 plt.subplot(122), plt.imshow(cv2.cvtColor(imagem_resultado, cv2.COLOR_BGR2RGB))
 plt.title('Objetos Detectados')
